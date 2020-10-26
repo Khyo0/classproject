0fbd87edaf8881007c60eecad3029cb1475a2c4b
Khyo0 <hyok860@gmail.com>
2020-10-26 17:18:40 +0900

    2020.10.26

--- java_project/PhoneBook/src/ver03/Util.java
+++ java_project/PhoneBook/src/ver03/Util.java
@@ -5,5 +5,5 @@ import java.util.Scanner;
 public class Util {
 
 	public static Scanner sc = new Scanner(System.in);
-	
+
 }
---
+++ java_project/PhoneBook/src/ver06/Util.java
@@ -0,0 +1,19 @@
+//package ver03;
+//
+//import java.util.Scanner;
+//
+//public class Util {
+//
+//	public static Scanner sc = new Scanner(System.in);
+//	
+//}
+
+package ver06;
+
+import java.util.Scanner;
+
+public interface Util {
+
+	Scanner SC = new Scanner(System.in);
+	
+}
